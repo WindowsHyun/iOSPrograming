b397142c731cfb0d71f5c76fa8245a95bcb0c017
WindowsHyun <kwon616@gmail.com>
2016-06-07 18:02:12 +0900

    Tap Me 이미지, 사운드 추가

---
+++ Tap Me Final/Tap Me/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  Tap Me
+//
+//  Created by Main Account on 10/3/14.
+//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var timerLabel: UILabel!
+    var count = 0
+    var seconds = 0
+    var timer = NSTimer()
+    
+    var buttonBeep:AVAudioPlayer?
+    var secondBeep:AVAudioPlayer?
+    var backgroundMusic:AVAudioPlayer?
+    
+    func setupAudioPlayerWithFile(file:NSString, type:NSString) -> AVAudioPlayer?  {
+        //1
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: type as String)
+        let url = NSURL.fileURLWithPath(path!)
+        
+        //2
+        var audioPlayer:AVAudioPlayer?
+        
+        // 3
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        } catch {
+            print("Player not available")
+        }
+        
+        return audioPlayer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let buttonBeep = self.setupAudioPlayerWithFile("ButtonTap", type:"wav") {
+            self.buttonBeep = buttonBeep
+        }
+        if let secondBeep = self.setupAudioPlayerWithFile("SecondBeep", type:"wav") {
+            self.secondBeep = secondBeep
+        }
+        if let backgroundMusic = self.setupAudioPlayerWithFile("HallOfTheMountainKing", type:"mp3") {
+            self.backgroundMusic = backgroundMusic
+        }
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        setupGame()
+        
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_tile.png")!)
+        
+        scoreLabel.backgroundColor = UIColor(patternImage: UIImage(named: "field_score.png")!)
+        timerLabel.backgroundColor = UIColor(patternImage: UIImage(named: "field_time.png")!)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonPressed()  {
+        count++
+        scoreLabel.text = "Score \n\(count)"
+        
+        buttonBeep?.play()
+    }
+    
+    func setupGame()  {
+        seconds = 30
+        count = 0
+        
+        timerLabel.text = "Time: \(seconds)"
+        scoreLabel.text = "Score: \(count)"
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("subtractTime"), userInfo: nil, repeats: true)
+        
+        backgroundMusic?.volume = 0.3
+        backgroundMusic?.play()
+    }
+    
+    func subtractTime() {
+        seconds--
+        timerLabel.text = "Time: \(seconds)"
+        
+        secondBeep?.play()
+        
+        if(seconds == 0)  {
+            timer.invalidate()
+            let alert = UIAlertController(title: "Time is up!",
+                                          message: "You scored \(count) points",
+                                          preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Play Again", style: UIAlertActionStyle.Default, handler: {
+                action in self.setupGame()
+            }))
+            presentViewController(alert, animated: true, completion:nil)
+        }
+    }
+    
+}
+
